3b97fa1c35720dba91e86c97031ffbc4e661ab8d
ignest <ignest.w@gmail.ru>
2021-09-04 21:01:14 +0400

    first commit

---
+++ src/main/java/ru/simbrsoft/toysstore/ToysStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.simbrsoft.toysstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToysStoreApplication.class, args);
+    }
+
+}
